30d92bca58f2f8ebb52e653c81dd4aede0059a8d
aimanyounises1 <aymanyounis@icloud.com>
2022-02-09 00:10:15 +0200

    added login page

--- wetravel/src/App.tsx
+++ wetravel/src/App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/Login";
+import { timeStamp } from "console";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Mainpage from "./components/Mainpage";
+document.title = "Travelers";
+const mainpage: boolean = false;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <Router>
+        <Link to="/">{!mainpage && <Login />}</Link>
+        <Link to="/mainpage">{mainpage && <Mainpage />}</Link>
+      </Router>
     </div>
   );
 }
