1a811ee0c33ed5fc40792635d7a2b3f653e69625
aimanyounises1 <aymanyounis@icloud.com>
2022-02-09 06:05:41 +0200

    added routers

--- wetravel/src/App.tsx
+++ wetravel/src/App.tsx
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/Login";
 import { timeStamp } from "console";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes , Route, Link } from "react-router-dom";
 import Mainpage from "./components/Mainpage";
 document.title = "Travelers";
 const mainpage: boolean = false;
@@ -11,8 +11,10 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Link to="/">{!mainpage && <Login />}</Link>
-        <Link to="/mainpage">{mainpage && <Mainpage />}</Link>
+      <Routes>
+        <Route path='/' element={<Login/>} ></Route>
+        <Route path="/mainpage" element={<Mainpage/>}></Route>
+      </Routes>
       </Router>
     </div>
   );
